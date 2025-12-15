1eac264c01f91faf3f4a0c3dc4b70e7e5c8c5260
rahat <57500825+Coder8128@users.noreply.github.com>
2025-12-15 21:07:28 +0100

    Add unfinished functionality to change grid size

--- index.js
+++ index.js
@@ -3,16 +3,40 @@ const sect = document.querySelector(".container");
 // div.className = "gridBlock";
 
 // sect.appendChild(div);
-let i = 0;
-while (i < 256) {
-    const div = document.createElement("div");
-    div.className = "gridBlock";
-    div.addEventListener("mouseover", (event) => {
-        console.log(event);
-        div.classList.add("hover");
-    });
+createGrid(20);
 
-    sect.appendChild(div);
-    console.log("1")
-    i++;
+function btnClick() {
+    sect.innerHTML = '';
+
+    let dimensions = prompt("Define grid size: ");
+    dimensions > 100 ? dimensions = prompt("Please less than 100") : null
+
+    if (dimensions < 100) {
+        createGrid(dimensions);
+    }
+    console.log(dimensions);
+}
+
+function createGrid(num) {
+    let i = 0;
+    let x = num * num;
+
+    while (i < num) {
+        const divRow = document.createElement("div");
+        divRow.className = "gridRow";
+        sect.appendChild(divRow);
+
+        for (let k = 0; k < num; k++) {
+            const div = document.createElement("div");
+            div.className = "gridBlock";
+            div.addEventListener("mouseover", (event) => {
+                console.log(event);
+                div.classList.add("hover");
+            });
+
+            divRow.appendChild(div);
+            console.log("count");
+        }
+        i++;
+    }
 }
